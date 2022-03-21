3d580ea867fa5eb2101d8f073f58720ab8d1650d
chenyuzou <chenyuzou@qq.com>
2022-03-21 21:15:02 +0800

    .

--- examples/sku.php
+++ examples/sku.php
@@ -7,14 +7,14 @@ use GuzzleHttp\Exception\GuzzleException;
 
 $client = new MeituanUnion\Client(KEY, SECRET, CALLBACK_SECRET);
 
-// 商品列表搜索接口
+// 由于技术功能升级，美团联盟优选业务【商品列表搜索接口(新版)】API接口将于2022年1月17日暂停数据访问，具体恢复时间另行通知。
 try {
     $request = new SkuQueryRequest();
-    $request->setBusinessType(6);   // 还是旧¢的业务线类型
+    $request->setBusinessType(6);   // 还是旧的业务线类型
     $request->setSid(SID);
-    $request->setPageSize(50);  // 该参数暂时无法生效，返回固定为20
+    $request->setPageSize(50);  // 该参数无法生效，返回固定为20
     $request->setPageNO(1);
-    $request->setLatitude('31.8240613040921'); // 武汉某地址
+    $request->setLatitude('31.8240613040921');
     $request->setLongitude('119.99997094482418');
     $request->setDeviceType('');
     $request->setDeviceId('');
--- src/BusinessLine.php
+++ src/BusinessLine.php
@@ -6,10 +6,8 @@ class BusinessLine
 {
     // 业务线类型 BusinessLine
     const PINGTAI = 1;  // 平台
-    const WAIMAI = 2;   // 外卖/闪购  子业务线：外卖=1  闪购=2
+    const WAIMAI = 2;   // 外卖/闪购 子业务线：外卖=1 闪购=2 团好货=3
     const HOTEL = 3;    // 酒店
     const YOUXUAN = 4;  // 优选
-
-    const SUB_LINE_WAIMAI = 1;
-    const SUB_LINE_SHANGOU = 2;
+    const NewUser = 5;  // 团APP拉新（地推类业务订单）
 }
